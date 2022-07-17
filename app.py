2a9c522352faccdb5c5fd9fb4a6a766186a2136d
yshah2306 <shah1b5@uwindsor.ca>
2022-07-17 15:11:53 -0400

    all

---
+++ app.py
@@ -0,0 +1,15 @@
+from email.mime import application
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route("/how_are_you")
+def hello():
+    return "I am good! How about you?"
+
+if __name__ == "__main__":
+    app.run()
