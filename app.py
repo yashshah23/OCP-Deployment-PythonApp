9f81fbe9b2b5b685b7c7501e3b2567a6d34ab73e
yshah2306 <shah1b5@uwindsor.ca>
2022-07-18 15:53:24 -0400

    lasttt

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-import os
+
 from flask import Flask
 app = Flask(__name__)
-from wsgiref.simple_server import make_server
 application = app
 
 @app.route("/")
@@ -13,8 +12,4 @@ def hello():
     return "I am good! How about you?"
 
 if __name__ == "__main__":
-    ip = os.environ['OPENSHIFT_PYTHON_IP'] 
-    port = os.environ['OPENSHIFT_PYTHON_PORT'] 
-    httpd = make_server(ip, port, application)
-    httpd.handle_request()
-    app.run(host = ip, port = port)
+    app.run()
